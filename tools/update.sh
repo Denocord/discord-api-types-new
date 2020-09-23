6df7f4b1f24845eb79e2afa63209961f4b66226e
TTtie <tttiett@outlook.com>
2020-09-23 17:14:09 +0200

    ✨ (tools): ask whether to commit
    Automatically committing the new information would make maintainers/users commit some possibly sensitive information and garbage files, including but not limited to .fuse_hiddenXXXXXXXXXXXX files (especially coming from GNU/Linux users using NTFS, which includes me as well)

--- tools/update.sh
+++ tools/update.sh
@@ -11,5 +11,10 @@ read REFORMAT
 if [[ $REFORMAT == "y" ]]; then
 deno fmt
 git add .
+echo "Would you like to create a commit?"
+read DO_COMMIT
+
+if [[ $DO_COMMIT == "y" ]]; then
 git commit -m "🎨 Format the updated code"
+fi
 fi
